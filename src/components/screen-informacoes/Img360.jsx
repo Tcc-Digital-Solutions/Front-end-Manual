27da2c07de1766cebe9f7d9d547f8297059a1846
realclownblack <danilowilliam3255@gmail.com>
2023-03-11 23:40:12 -0300

    11/03/2023 - 23:39

--- src/components/screen-informacoes/Img360.jsx
+++ src/components/screen-informacoes/Img360.jsx
@@ -6,7 +6,7 @@ export const Img360 = () => {
     return (
         <div className="img360-div" >
             <span className="img360-produto-name">Parafusadeira GSB 13RE</span>
-            <iframe className='img360-iframe' src="https://360.boschimages.com/G0000BWXjvadoXHA/embed" width="100%" height="100%"></iframe>
+            <iframe className='img360-iframe' src="https://360.boschimages.com/G0000BWXjvadoXHA/embed" width="100%" height="100%" scrolling="no" id="frame-spec"></iframe>
         </div>
     )
 }
