476022d13f7847fd9f4b671c3b49b0b6a285a032
Alves Bianca (CaP/ETS) <abl3ca@bosch.com>
2023-04-03 11:27:37 -0300

    atualização pagina home

--- src/components/screen-home/Cardproduto.jsx
+++ src/components/screen-home/Cardproduto.jsx
@@ -5,10 +5,8 @@ import ferramenta from '../../assets/img/f12-chave-menu.png'
 export const Cardproduto = (props) => {
     return (
         <div className="Cardproduto-div" >
-            <span className='Cardproduto-div-01'>
-            </span>
             <span className='Cardproduto-div-02'>
-                <img src={ferramenta} alt="" />
+                <img className='img-card' src={ferramenta} alt="" />
                 <span className='Cardproduto-div-03'>
                     <span className='Cardproduto-div-text'>Parafusadeira Furadeira GSB 18V-50</span>
                     <button className='Cardproduto-button'>Manual</button>                    
--- src/components/screen-home/NavbarHome.jsx
+++ src/components/screen-home/NavbarHome.jsx
@@ -5,8 +5,6 @@ import { useState } from 'react'
 
 
 export const NavbarHome = () => {
-    const [visible, setVisible] = useState("box-categoria-span-fechar")
-    const [pressed, setPressed] = useState(false)
 
     const [visible1, setVisible1] = useState("box-categoria-span-fechar")
     const [pressed1, setPressed1] = useState(false)
@@ -20,45 +18,30 @@ export const NavbarHome = () => {
     const [visible4, setVisible4] = useState("box-categoria-span-fechar")
     const [pressed4, setPressed4] = useState(false)
 
-    const [visible5, setVisible5] = useState("box-categoria-span-fechar")
-    const [pressed5, setPressed5] = useState(false)
+    const setMenuVisible = (num) => {
+        setCategoria(1)
+        if (num == 1){
+            pressed1 ? setVisible1("box-categoria-span-fechar") : setVisible1("box-categoria-span")
+            setPressed1(!pressed1)
+        }
+        if (num == 2){
+            pressed2 ? setVisible2("box-categoria-span-fechar") : setVisible2("box-categoria-span")
+            setPressed2(!pressed2)
+        }
+        if (num == 3){
+         
+            pressed3 ? setVisible3("box-categoria-span-fechar") : setVisible3("box-categoria-span")
+            setPressed3(!pressed3)
+        }
 
-    const setMenuVisible = () => {
-        pressed ? setVisible("box-categoria-span-fechar") : setVisible("box-categoria-span")
-        setPressed(!pressed)
-
+        if (num == 4){
+            pressed4 ? setVisible4("box-categoria-span-fechar") : setVisible4("box-categoria-span")
+            setPressed4(!pressed4)
+           
+        }
     }
-    const setMenuVisible1 = () => {
-
-        pressed1 ? setVisible1("box-categoria-span-fechar") : setVisible1("box-categoria-span")
-        setPressed1(!pressed1)
-    }
-
-    const setMenuVisible2 = () => {
-
-        pressed2 ? setVisible2("box-categoria-span-fechar") : setVisible2("box-categoria-span")
-        setPressed2(!pressed2)
-    }
-
-    const setMenuVisible3 = () => {
-
-        pressed3 ? setVisible3("box-categoria-span-fechar") : setVisible3("box-categoria-span")
-        setPressed3(!pressed3)
-    }
-
-    const setMenuVisible4 = () => {
-
-        pressed4 ? setVisible4("box-categoria-span-fechar") : setVisible4("box-categoria-span")
-        setPressed4(!pressed4)
-    }
-
-    const setMenuVisible5 = () => {
-
-        pressed5 ? setVisible5("box-categoria-span-fechar") : setVisible5("box-categoria-span")
-        setPressed5(!pressed5)
-    }
-
-    const [categoria, setCategoria] = useState(0)
+    
+    const [categoria, setCategoria] = useState(1)
 
     const exibirProdutos = (categoria) => {
         setCategoria(categoria)
@@ -68,12 +51,11 @@ export const NavbarHome = () => {
         <div className="box-categoria-div" >
             <ul className="box-categoria-ul">
                 <li className="box-categoria-li" id='li-categorias'>
-                    <button className="box-categoria-button" onClick={() => setMenuVisible()} onBlur={() => setMenuVisible("box-categoria-span-fechar")} >Ferramentas 12v</button>
-                </li>
-                <span className={visible}>
+                    <button className="box-categoria-button" onClick={() => setMenuVisible(1)} onBlur={() => setMenuVisible(1)} >Ferramentas 12v</button>
+                    <span className={visible1}>
                     <div className='box-menu'>
                         <ul>
-                            <li onMouseOver={() => exibirProdutos(1)}>Furadeiras e Parafusadeiras</li>
+                            <li className={categoria == 1 ? 'active' : null} onMouseOver={() => exibirProdutos(1)}>Furadeiras e Parafusadeiras</li>
                             <li onMouseOver={() => exibirProdutos(2)}>Chave de Impacto</li>
                             <li onMouseOver={() => exibirProdutos(3)}>Serra Circular</li>
                             <li onMouseOver={() => exibirProdutos(4)}>Serra Tico-Tico</li>
@@ -137,13 +119,14 @@ export const NavbarHome = () => {
                     </div>
                     
                 </span>
-                <li className="box-categoria-li" id='li-categorias'>
-                    <button className="box-categoria-button" onClick={() => setMenuVisible1()} onBlur={() => setMenuVisible1("box-categoria-span-fechar")}>Ferramentas 18v </button>
                 </li>
-                <span className={visible1}>
+                
+                <li className="box-categoria-li" id='li-categorias'>
+                    <button className="box-categoria-button" onClick={() => setMenuVisible(2)} onBlur={() => setMenuVisible(2)}>Ferramentas 18v </button>
+                    <span className={visible2}>
                 <div className='box-menu'>
                         <ul>
-                            <li onMouseOver={() => exibirProdutos(1)}>Furadeiras e Parafusadeiras</li>
+                            <li className={categoria == 1 ? 'active' : null} onMouseOver={() => exibirProdutos(1)}>Furadeiras e Parafusadeiras</li>
                             <li onMouseOver={() => exibirProdutos(2)}>Chave de Impacto</li>
                             <li onMouseOver={() => exibirProdutos(3)}>Serra Circular e Serra Tico-Tico</li>
                             <li onMouseOver={() => exibirProdutos(4)}>Plaina</li>
@@ -212,13 +195,15 @@ export const NavbarHome = () => {
                     </div>
                 </span>
                 
-                <li className="box-categoria-li" id='li-categorias'>
-                    <button className="box-categoria-button" onClick={() => setMenuVisible4()} onBlur={() => setMenuVisible4("box-categoria-span-fechar")}>Linha de Medição </button>
                 </li>
-                <span className={visible4}>
+               
+                
+                <li className="box-categoria-li" id='li-categorias'>
+                    <button className="box-categoria-button" onClick={() => setMenuVisible(3)} onBlur={() => setMenuVisible(3)}>Linha de Medição </button>
+                    <span className={visible3}>
                 <div className='box-menu'>
                         <ul>
-                            <li onMouseOver={() => exibirProdutos(1)}>Niveladores</li>
+                            <li className={categoria == 1 ? 'active' : null} onMouseOver={() => exibirProdutos(1)}>Niveladores</li>
                             <li onMouseOver={() => exibirProdutos(2)}>Medidores</li>
                             <li onMouseOver={() => exibirProdutos(3)}>Detectores</li>
                             <li onMouseOver={() => exibirProdutos(4)}>Acessórios</li>
@@ -251,13 +236,18 @@ export const NavbarHome = () => {
                         
                     </div>
                 </span>
-                <li className="box-categoria-li" id='li-categorias'>
-                    <button className="box-categoria-button" onClick={() => setMenuVisible5()} onBlur={() => setMenuVisible5("box-categoria-span-fechar")}>Acessórios</button>
                 </li>
-                <span className={visible5}>
+                
+                <li className="box-categoria-li" id='li-categorias'>
+                    <button className="box-categoria-button" onClick={() => setMenuVisible(4)}
+                    //  onBlur={() => setMenuVisible(4)}
+                     >Acessórios</button>
+                     {/* flex-direction: row-reverse;
+                     margin-left: -300px; */}
+                    <span className={visible4}>
                 <div className='box-menu'>
                         <ul>
-                            <li onMouseOver={() => exibirProdutos(1)}>Perfurar e Parafusar</li>
+                            <li className={categoria == 1 ? 'active' : null} onMouseOver={() => exibirProdutos(1)}>Perfurar e Parafusar</li>
                             <li onMouseOver={() => exibirProdutos(2)}>Serrar</li>
                             <li onMouseOver={() => exibirProdutos(3)}>Cortar e Desbastar</li>
                             <li onMouseOver={() => exibirProdutos(4)}>Acabamento</li>
@@ -291,6 +281,8 @@ export const NavbarHome = () => {
                         
                     </div>
                 </span>
+                </li>
+                
             </ul>
 
         </div>
