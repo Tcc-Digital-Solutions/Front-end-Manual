c546608515e0894478c7d3aaa02fc7160cf88945
MoreiraSte <casagrandestephany91@gmail.com>
2023-04-04 11:11:29 -0300

    integration finished 04-04-23

--- src/components/universal/ButtonsBar.jsx
+++ src/components/universal/ButtonsBar.jsx
@@ -1,16 +1,36 @@
 import '../../assets/css/css_universal/Buttons_bar.css'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { ButtonAcessibilidade } from './ButtonAcessibilidade';
 
+
+
+
 export const ButtonsBar = () => {
+
+    // const [exibir,setExibir] = useState(false);
+    const navigate = useNavigate()
+
+// const sleep = ms => new Promise(r => setTimeout(r, ms));
+//     const teste = async() => {
+//         await sleep(2000)
+//         setExibir(true)
+//     }
+   
+//     teste()
+
     return (
         <div className="buttons-bar-div" >
             <div className="buttons-bar-ilha">
                 <span className="buttons-bar-ilha-button-acessibilidade"> <ButtonAcessibilidade /></span>
-                <Link to={'/manual'} className="buttons-bar-click-link"><button className="buttons-bar-click" role="button">Manual</button></Link>
-                <Link to={'/montagem'} className="buttons-bar-click-link"><button className="buttons-bar-click" role="button">Montagem</button></Link>
-                <Link to={'/informacoes'} className="buttons-bar-click-link"><button className="buttons-bar-click" role="button">informações</button></Link>
+                {/* <Link to={'/manual'} className="buttons-bar-click-link"><button className="buttons-bar-click" role="button">Manual</button></Link> */}
+               <button onClick={() => navigate('/manual')} className="buttons-bar-click" role="button">Manual</button>
+                <button onClick={() => navigate('/montagem')} className="buttons-bar-click" role="button">Montagem</button>
+                <button onClick={() => navigate('/informacoes')} className="buttons-bar-click" role="button">informações</button>
             </div>
+
+            {/* {exibir?  */}
+            {/* <VLibras forceOnload={true}  /> */}
+            {/* :null} */}
         </div>
     )
 }
