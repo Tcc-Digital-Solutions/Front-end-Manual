79de33d24d4e6dd93c90f98f6b6411990a5490b1
TifanyAlmeida <ticaanahi@outlook.com>
2023-05-19 15:36:20 -0300

    15:36 inserts bd atualizados

--- Front-end-manual/src/pages/Montagem.jsx
+++ Front-end-manual/src/pages/Montagem.jsx
@@ -1,27 +1,19 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import { BoxMontagem } from '../components/screen-montagem/BoxMontagem';
 import { ButtonsBar } from '../components/universal/ButtonsBar';
 import { Navbar } from '../components/universal/Navbar';
 
-const Montagem = () =>{
-    const [description, setDescrition] = useState()
-
-    useEffect(() => {
-        const produtoInfo = localStorage.getItem('produtoInfo')
-        if (produtoInfo != undefined && produtoInfo != null){
-            const prodInfoJSON = JSON.parse(produtoInfo)
-            setDescrition(prodInfoJSON.description.split('<br/>'))
-        }
-        
-    },[localStorage.getItem('produtoInfo')])
-    // console.log('descricao', description.description)
-    return (
-        <>
-            <Navbar visible='button-menu-box-fechar' search='button-menu-box-fechar'  box='button-menu-box-fechar' nave="navbar-div"/>
-            <BoxMontagem />
-            <ButtonsBar />
-        </>
-    );
+class Montagem extends Component {
+    state = {}
+    render() {
+        return (
+            <>
+                <Navbar visible='button-menu-box-fechar' search='button-menu-box-fechar'  box='button-menu-box-fechar' nave="navbar-div"/>
+                <BoxMontagem />
+                <ButtonsBar />
+            </>
+        );
+    }
 }
 
 export default Montagem;
